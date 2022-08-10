cc286d7ce9ffcbe30e0890c2e9d20f71110d47df
Jescience <110980289+Jescience@users.noreply.github.com>
2022-08-10 10:19:24 -0500

    Create ultrasonic_only.py

---
+++ ultrasonic_only.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPI.BCM)
+GPIO.setwarnings(False)
+
+trig=23
+echo=24
+GPIO.setup(echo, GPIO.IN)
+GPIO.setup(trig, GPIO.OUT)
+
+while True:
+  GPIO.output(trig, True)
+  time.sleep(0.00001) # 10 microseconds
+  GPIO.output(trig, False)
+  while GPIO.input(echo) == 0:
+    pass
+  start = time.time()
+  while GPIO.input(echo) == 1:
+    pass
+  end = time.time()
+  distance = ((end - start) * 34300) / 2
+  print("distance:", distance, "cm")
+  time.sleep(1)
